a5af89ec21905fa04e384ac379f6d0bcc61ca27c
luke <luke.stewart@snap.net.nz>
2015-05-13 11:28:09 +1200

    Initial version of read budget script
    - Reads Budget from xls worksheet and imports it into psql using psycopg executemany
    - creates a table name as specific in variable tablename
    - column names based on row 1 of xls budget data file
      - replaces spaces for _ and removes anything after a (
    - can handle data of type text, int or float
      - if a column has any non intergers in it creates as a double precision
    
    TODO
    - check unicode handling

--- src/read_budget.py
+++ src/read_budget.py
@@ -5,22 +5,30 @@
 
 import xlrd
 import sys
+import psycopg2
 
 
 inputfilename = "../data/b14-expenditure-data.xls"
 datasheetname= 'Raw Data'
+tablename = 'raw_data'
 
 tnumeric = 'NUMERIC'
 tvarchar = 'VARCHAR'
 types = {0:None,1:tvarchar,2:tnumeric,3:'DATE',4:'BOOLEAN',5:'ERROR'}
 
 
-def create_table(data):
+def create_table(data,curr):
     # cols holds tuples (col_index_in_sheet,col_name,col_postgres_type,col_max_length)
     # col_max_length non zero for types of varchar
+    curr.execute("drop table if exists %s CASCADE" % tablename)
     cols = []
     for index in range(data.ncols):        
         col_name = data.cell(0,index).value
+        if "(" in col_name:
+            col_name = col_name[:col_name.find("(")]
+        col_name = col_name.strip()
+        col_name = col_name.replace(" ", "_")
+
         col_type = 0
         col_postgres_type = ""
         col_max_length = None
@@ -47,16 +55,40 @@ def create_table(data):
                 col_postgres_type = 'DOUBLE PRECISION'
         
         cols.append((index,col_name,col_postgres_type, col_max_length))
-    ins_str = "CREATE TABLE tablename(\n\t"
-    ins_str += "tid".ljust(25) + "\tSERIAL"
+    ins_str = "CREATE TABLE %s (" %tablename
+    ins_str += "tid" + " SERIAL"
     for c in cols:
-        ins_str += ",\n\t"
-        ins_str += c[1].ljust(25) + "\t" + c[2]
+        ins_str += ", "
+        ins_str += c[1] + " " + c[2]
         if c[2] == tvarchar:
             ins_str+= "(%d)" % c[3]
-    ins_str = ins_str + "\n);"
-    return ins_str
-def main():
+    ins_str = ins_str + ");"
+    curr.execute(ins_str)
+    return cols
+
+
+
+
+def load_data(data,cols, curr):
+    ins_str = "INSERT INTO %s(" % tablename
+    for col in cols:
+        ins_str += "%s," % col[1]
+    ins_str = ins_str[:-1] +") VALUES (" 
+    for col in cols:
+        ins_str += "%s,"
+    ins_str = ins_str[:-1] + ")"
+
+    input_data = []
+    row = 1
+    for row in range(1,data.nrows):
+        input_data.append(data.row_values(row))
+
+    print "Read Data"
+    print "Loading into Postgresql"
+    curr.executemany(ins_str,input_data)
+    
+
+def main(curr):
     book = xlrd.open_workbook(inputfilename)
 
     # Confirm presences of data sheet
@@ -66,10 +98,24 @@ def main():
         sys.exit(1)
         
     data = book.sheet_by_name(datasheetname)
-    print create_table(data)
-        
+    cols = create_table(data,curr)
+    load_data(data,cols,curr)
     
+
     
 
 if __name__ == "__main__":
-    main()
+
+    dbname='budget'
+    user='python'
+    host='192.168.20.106'
+    password='ReddEft7'
+    conn_str = "dbname='%s' user='%s' host='%s' password='%s'" % (dbname, user, host, password)
+    conn = psycopg2.connect(conn_str)       
+    curr = conn.cursor()
+    main(curr)
+    conn.commit()
+        
+        
+        
+
